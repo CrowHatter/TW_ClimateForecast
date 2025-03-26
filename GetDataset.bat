bae2a9cffdeeb27d7622c3497385cec3d7b3ba66
Pointer <ericweng92@gmail.com>
2025-03-26 22:21:29 +0800

    Update GetDataset.bat to execute new data processing scripts

--- GetDataset.bat
+++ GetDataset.bat
@@ -6,7 +6,10 @@ REM 啟動虛擬環境
 call ".venv\Scripts\activate.bat"
 
 REM 執行 Python 腳本
-python GetDataset.py
+python GetDataset_HumanStation.py
+
+REM
+python GetDataset_NoHumanStation.py
 
 REM 可選：停用虛擬環境（若有需要，可加入 deactivate 命令）
 deactivate
