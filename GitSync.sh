f5d5f26545dec39753cde8c438198d55b5000722
ericwengRaspberryPi <ericweng92@gmail.com>
2025-03-31 15:46:03 +0800

    Auto commit on 2025-03-31 15:46:03

--- GitSync.sh
+++ GitSync.sh
@@ -17,8 +17,10 @@ git commit -m "Auto commit on $(date '+%Y-%m-%d %H:%M:%S')"
 # 推送到 GitHub
 git push
 
+# modify http to ssh
 # git remote set-url origin git@github.com:CrowHatter/TW_ClimateForecast.git
 
+# crontab -e
 # chmod +x /home/ericweng/Desktop/TW_ClimateForecast/GitSync.sh
 # 30 15 * * * /home/ericweng/Desktop/TW_ClimateForecast/GitSync.sh >> /home/ericweng/Desktop/TW_ClimateForecast/logs/git_sync.log 2>&1
 
